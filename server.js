979f4f8233c06693bebfc9c9d6ba049972786c5f
Atruo <banyuls98@gmail.com>
2018-10-09 18:22:52 +0200

    Lista usuarios

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const io = socketIO(server);
 const path = require("path");
 
 var users = {};
+var usuarios = [];
+var cont = 0;
 var name = '';
 console.log('Server funcionando...');
 console.log('Para ejecutar el link remoto hacer: ./(la barra hacia el otro lado)ngrok http 3000');
@@ -19,17 +21,23 @@ app.get('/:name', function(req, res){//Una vez tenemos un usuario mandamos el ch
 
 
 // socket
-io.sockets.on("connection", function(socket){
+io.sockets.on("connection", function(socket){//Conectamos el socket
 
-    users[socket.id] = name;
+    users[socket.id] = name;//Array de usuarios
     socket.on("nRoom", function(room){
         socket.join(room);
-        socket.broadcast.in(room).emit("node new user", users[socket.id] + " new user has joined");
+        socket.broadcast.in(room).emit("node new user", "Se acaba de unir al chat: "+ users[socket.id]);//Nuevo usuarios
+        if (users[socket.id] != '') {
+          usuarios[cont] = users[socket.id];
+          io.sockets.in("nRoom").emit('actualizar usuarios', usuarios);
+          cont++;
+        }
+
     });
 
-    socket.on("node new message", function(data){
+    socket.on("node new message", function(data){//Si recibe un nuevo mensaje
         io.sockets.in("nRoom").emit('node news', users[socket.id] + ": "+ data);
     });
-    
+
 
 });
